fcf7385e3cebe8079f55b656cd5fe67ea85ddd17
twicks95 <teguh.wck95@gmail.com>
2021-06-18 16:01:02 +0700

    Fix init project issues

--- next.config.js
+++ next.config.js
@@ -1,3 +1,32 @@
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    APP_NAME: "Coffee Mate",
+    API_BASE_URL: "http://localhost:3004/backend5/api/v1/",
+    API_IMG_URL: "http://localhost:3004/backend5/api/",
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/login",
+        destination: "/auth/login",
+      },
+      {
+        source: "/signup",
+        destination: "/auth/signup",
+      },
+      {
+        source: "/create-pin",
+        destination: "/auth/create-pin",
+      },
+      {
+        source: "/create-pin/success",
+        destination: "/auth/create-pin/success",
+      },
+      {
+        source: "/reset-password",
+        destination: "/auth/reset-password",
+      },
+    ];
+  },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+// import "../styles/globals.css";
+// import "bootstrap/dist/css/bootstrap.css";
 
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+// }
+
+// export default MyApp;
+
+import Head from "next/head";
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { Provider } from "react-redux";
+//==================================
+// import { useStore } from "redux/store";
+//==================================
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  //==================================
+  // const store = useStore(pageProps.initialReduxState);
+  //==================================
+  return (
+    <>
+      {/* <Provider store={store}> */}
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+      {/* </Provider> */}
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
