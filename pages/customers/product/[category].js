8d02d3daf924bfb2e1371a5c17cbe7625828a44e
twicks95 <teguh.wck95@gmail.com>
2021-06-23 02:08:22 +0700

    Add search, sorting, choose category

--- pages/customers/product/[category].js
+++ pages/customers/product/[category].js
@@ -1,21 +1,97 @@
 /* eslint-disable @next/next/no-img-element */
 import Layout from "components/Layout";
 import { useState } from "react";
-import { Button, Col, Row } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  Dropdown,
+  DropdownButton,
+  Form,
+  Row,
+} from "react-bootstrap";
 import styles from "styles/CustProducts.module.css";
 import Image from "next/image";
 import Navbar from "components/module/Navbar";
 import Footer from "components/module/Footer";
+import { authPage, customerPage } from "middleware/authPage";
+import { useRouter } from "next/router";
+import axiosApiIntances from "utils/axios";
+import ReactPaginate from "react-paginate";
 
-export default function Product() {
+export const getServerSideProps = async (context) => {
+  const data = await authPage(context);
+  await customerPage(context);
+  let { category } = context.params;
+  let { keyword, order, page } = context.query;
+
+  const promos = await axiosApiIntances
+    .get("promo", {
+      headers: {
+        Authorization: `Bearer ${data.token || ""}`,
+      },
+    })
+    .then((res) => {
+      return res.data.data;
+    });
+
+  if (category === "all") {
+    category = "";
+  }
+  if (!keyword) {
+    keyword = "";
+  }
+  if (!order) {
+    order = "";
+  }
+  if (!page) {
+    page = "1";
+  }
+  const products = await axiosApiIntances
+    .get(
+      `product/category?category=${category}&keyword=${keyword}&orderBy=${order}&page=${page}`,
+      {
+        headers: {
+          Authorization: `Bearer ${data.token || ""}`,
+        },
+      }
+    )
+    .then((res) => {
+      return res.data;
+    })
+    .catch(() => {
+      return { data: [], pagination: [] };
+    });
+  return {
+    props: { pagination: products.pagination, products: products.data, promos },
+  };
+};
+
+export default function Product(props) {
+  const router = useRouter();
+  const [page, setPage] = useState("1");
+  const [order, setOrder] = useState("");
+  const [keyword, setKeyword] = useState("");
   const [selectedCoupon, setSelectedCoupon] = useState({});
-  const [selectedCategory, setSelectedCategory] = useState({ favourite: true });
+  const [selectedCategory, setSelectedCategory] = useState("all");
 
   const handleSelectCoupon = (e) => {
-    setSelectedCoupon({ [e.target.id]: true });
+    setSelectedCoupon({
+      [e.target.id]: true,
+    });
   };
-  const handleSelectCategory = (e) => {
-    setSelectedCategory({ [e.target.id]: true });
+
+  const handleSearch = () => {
+    router.push(
+      `/customers/product/${selectedCategory}?keyword=${keyword}&order=${order}&page=${page}`
+    );
+  };
+
+  const handlePageClick = (e) => {
+    const selectedPage = e.selected + 1;
+    setPage(selectedPage);
+    router.push(
+      `/customers/product/${selectedCategory}?keyword=${keyword}&order=${order}&page=${selectedPage}`
+    );
   };
 
   return (
@@ -29,65 +105,29 @@ export default function Product() {
           </div>
           <div className={styles.coupons}>
             {/* LOOPING HERE */}
-            <div
-              id="c1"
-              className={`${styles.coupon} ${
-                selectedCoupon.c1 && styles.selectedCoupon
-              }`}
-              onClick={(e) => handleSelectCoupon(e)}
-            >
-              <img id="c1" src="/sketch1.png" alt="coupon-sketch" />
-              <div id="c1">
-                <h6 id="c1">{"HAPPY MOTHER'S DAY"}</h6>
-                <span id="c1">Get one of our favourite menu for free!</span>
+            {props.promos.map((item, index) => (
+              <div
+                id={`c${item.promo_id}`}
+                className={`${styles.coupon} ${
+                  `
+                  ${selectedCoupon.c}${item.promo_id}` && styles.selectedCoupon
+                }`}
+                key={item.promo_id}
+                onClick={(e) => handleSelectCoupon(e)}
+              >
+                {item.promo_image && (
+                  <img
+                    id={`c${item.promo_id}`}
+                    src={`${process.env.API_IMG_URL}${item.promo_image}`}
+                    alt="coupon-image"
+                  />
+                )}
+                <div id={`c${item.promo_id}`}>
+                  <h6 id={`c${item.promo_id}`}>{item.promo_name}</h6>
+                  <span id={`c${item.promo_id}`}>{item.promo_desc}</span>
+                </div>
               </div>
-            </div>
-            <div
-              id="c2"
-              style={{ backgroundColor: "#F5C361" }}
-              className={`${styles.coupon} ${
-                selectedCoupon.c2 && styles.selectedCoupon
-              }`}
-              onClick={(e) => handleSelectCoupon(e)}
-            >
-              <img id="c2" src="/sketch2.png" alt="coupon-sketch" />
-              <div id="c2">
-                <h6 id="c2">
-                  {"Get a cup of coffee for free on sunday morning"}
-                </h6>
-                <span id="c2">Only at 7 to 9 AM</span>
-              </div>
-            </div>
-            <div
-              id="c3"
-              className={`${styles.coupon} ${
-                selectedCoupon.c3 && styles.selectedCoupon
-              }`}
-              onClick={(e) => handleSelectCoupon(e)}
-            >
-              <img id="c3" src="/sketch1.png" alt="coupon-sketch" />
-              <div id="c3">
-                <h6 id="c3">{"HAPPY MOTHER'S DAY"}</h6>
-                <span id="c3">Get one of our favourite menu for free!</span>
-              </div>
-            </div>
-            <div
-              id="c4"
-              style={{ backgroundColor: "#C59378" }}
-              className={`${styles.coupon} ${
-                selectedCoupon.c4 && styles.selectedCoupon
-              }`}
-              onClick={(e) => handleSelectCoupon(e)}
-            >
-              <img id="c4" src="/sketch3.png" alt="coupon-sketch" />
-              <div id="c4">
-                <h6 id="c4">{"HAPPY HALLOWEEN"}</h6>
-                <span id="c4">
-                  Do you like chicken wings? Get 1 free only if you buy pinky
-                  promise
-                </span>
-              </div>
-            </div>
+            ))}
           </div>
           <Button variant="secondary">Apply Coupon</Button>
           <section className={styles.termsCondition}>
@@ -103,165 +143,160 @@ export default function Product() {
         <section className={styles.products}>
           <div className={styles.category}>
             <h4
-              id="favourite"
-              className={selectedCategory.favourite && styles.activeCategory}
-              onClick={(e) => handleSelectCategory(e)}
+              id="all"
+              className={
+                selectedCategory === "all" ? styles.activeCategory : ""
+              }
+              onClick={(e) => {
+                setSelectedCategory(e.target.id),
+                  router.push(`/customers/product/${e.target.id}?page=${page}`);
+              }}
             >
-              Favourite Product
+              All Products
             </h4>
             <h4
               id="coffee"
-              className={selectedCategory.coffee && styles.activeCategory}
-              onClick={(e) => handleSelectCategory(e)}
+              className={
+                selectedCategory === "coffee" ? styles.activeCategory : ""
+              }
+              onClick={(e) => {
+                setSelectedCategory(e.target.id),
+                  router.push(`/customers/product/${e.target.id}?page=${page}`);
+              }}
             >
               Coffee
             </h4>
             <h4
               id="nonCoffee"
-              className={selectedCategory.nonCoffee && styles.activeCategory}
-              onClick={(e) => handleSelectCategory(e)}
+              className={
+                selectedCategory === "nonCoffee" ? styles.activeCategory : ""
+              }
+              onClick={(e) => {
+                setSelectedCategory(e.target.id),
+                  router.push(`/customers/product/${e.target.id}?page=${page}`);
+              }}
             >
               Non Coffee
             </h4>
             <h4
-              id="foods"
-              className={selectedCategory.foods && styles.activeCategory}
-              onClick={(e) => handleSelectCategory(e)}
+              id="food"
+              className={
+                selectedCategory === "food" ? styles.activeCategory : ""
+              }
+              onClick={(e) => {
+                setSelectedCategory(e.target.id),
+                  router.push(`/customers/product/${e.target.id}?page=${page}`);
+              }}
             >
               Foods
             </h4>
             <h4
               id="addOn"
-              className={selectedCategory.addOn && styles.activeCategory}
-              onClick={(e) => handleSelectCategory(e)}
+              className={
+                selectedCategory === "addOn" ? styles.activeCategory : ""
+              }
+              onClick={(e) => {
+                setSelectedCategory(e.target.id),
+                  router.push(`/customers/product/${e.target.id}?page=${page}`);
+              }}
             >
               Add-On
             </h4>
           </div>
+          <div className={styles.searchBar}>
+            <Form.Control
+              size="sm"
+              type="text"
+              placeholder="Small text"
+              className="shadow-none"
+              onChange={(e) => setKeyword(e.target.value)}
+              onKeyUp={(e) => e.key === "Enter" && handleSearch()}
+            />
+            <DropdownButton
+              id="dropdown-basic-button"
+              menuAlign="right"
+              title="Sort By"
+              value={keyword}
+            >
+              <Dropdown.Item onClick={() => setOrder("product_name ASC")}>
+                Name a-Z
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => setOrder("product_name DESC")}>
+                Name A-z
+              </Dropdown.Item>
+              <Dropdown.Item onClick={() => setOrder("product_base_price ASC")}>
+                Price 0-100
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => setOrder("product_base_price DESC")}
+              >
+                Price 100-0
+              </Dropdown.Item>
+            </DropdownButton>
+          </div>
           <main className={styles.productList}>
             <Row xs={2} md={4} className={`gx-3 ${styles.row}`}>
-              <Col>
-                <div className={styles.cardProduct}>
-                  <div className={styles.imgContainer}>
-                    <Image
-                      layout="fill"
-                      src="/product1.png"
-                      alt="product-img"
-                    />
+              {props.products.length === 0 ? (
+                <Col style={{ width: "100%" }}>
+                  <div
+                    style={{
+                      display: "grid",
+                      height: "20vh",
+                      placeItems: "center",
+                    }}
+                  >
+                    <strong style={{ fontSize: "2em" }}>No Products</strong>
                   </div>
-                  <div className="d-flex flex-column align-items-center">
-                    <h5>Veggie tomato mix</h5>
-                    <span>IDR 34.000</span>
-                  </div>
-                </div>
-              </Col>
-              <Col>
-                <div className={styles.cardProduct}>
-                  <div className={styles.imgContainer}>
-                    <Image
-                      layout="fill"
-                      src="/product6.png"
-                      alt="product-img"
-                    />
-                  </div>
-                  <div className="d-flex flex-column align-items-center">
-                    <h5>Hazelnut Latte</h5>
-                    <span>IDR 25.000</span>
-                  </div>
-                </div>
-              </Col>
-              <Col>
-                <div className={styles.cardProduct}>
-                  <div className={styles.imgContainer}>
-                    <Image
-                      layout="fill"
-                      src="/product2.png"
-                      alt="product-img"
-                    />
-                  </div>
-                  <div className="d-flex flex-column align-items-center">
-                    <h5>Summer fried rice</h5>
-                    <span>IDR 32.000</span>
-                  </div>
-                </div>
-              </Col>
-              <Col>
-                <div className={styles.cardProduct}>
-                  <div className={styles.imgContainer}>
-                    <Image
-                      layout="fill"
-                      src="/product3.png"
-                      alt="product-img"
-                    />
-                  </div>
-                  <div className="d-flex flex-column align-items-center">
-                    <h5>Creamy Ice Latte</h5>
-                    <span>IDR 27.000</span>
-                  </div>
-                </div>
-              </Col>
-              <Col>
-                <div className={styles.cardProduct}>
-                  <div className={styles.imgContainer}>
-                    <Image
-                      layout="fill"
-                      src="/product5.png"
-                      alt="product-img"
-                    />
-                  </div>
-                  <div className="d-flex flex-column align-items-center">
-                    <h5>Drum Sticks</h5>
-                    <span>IDR 30.000</span>
-                  </div>
-                </div>
-              </Col>
-              <Col>
-                <div className={styles.cardProduct}>
-                  <div className={styles.imgContainer}>
-                    <Image
-                      layout="fill"
-                      src="/product4.png"
-                      alt="product-img"
-                    />
-                  </div>
-                  <div className="d-flex flex-column align-items-center">
-                    <h5>Salty Rice</h5>
-                    <span>IDR 20.000</span>
-                  </div>
-                </div>
-              </Col>
-              <Col>
-                <div className={styles.cardProduct}>
-                  <div className={styles.imgContainer}>
-                    <Image
-                      layout="fill"
-                      src="/product2.png"
-                      alt="product-img"
-                    />
-                  </div>
-                  <div className="d-flex flex-column align-items-center">
-                    <h5>Summer fried rice</h5>
-                    <span>IDR 32.000</span>
-                  </div>
-                </div>
-              </Col>
-              <Col>
-                <div className={styles.cardProduct}>
-                  <div className={styles.imgContainer}>
-                    <Image
-                      layout="fill"
-                      src="/product1.png"
-                      alt="product-img"
-                    />
-                  </div>
-                  <div className="d-flex flex-column align-items-center">
-                    <h5>Veggie tomato mix</h5>
-                    <span>IDR 34.000</span>
-                  </div>
-                </div>
-              </Col>
+                </Col>
+              ) : (
+                props.products.map((item, index) => (
+                  <Col
+                    key={index}
+                    onClick={() =>
+                      router.push(
+                        `/customers/product-details/${item.product_id}`
+                      )
+                    }
+                  >
+                    <div className={styles.cardProduct}>
+                      <div className={styles.imgContainer}>
+                        <Image
+                          layout="fill"
+                          src={
+                            item.product_image
+                              ? `${process.env.API_IMG_URL}${item.product_image}`
+                              : "/default-img-placeholder.png"
+                          }
+                          alt="product-img"
+                        />
+                      </div>
+                      <div className="d-flex flex-column align-items-center">
+                        <h5>{item.product_name}</h5>
+                        <span>IDR {item.product_base_price}</span>
+                      </div>
+                    </div>
+                  </Col>
+                ))
+              )}
             </Row>
           </main>
+          <div
+            className={`d-flex justify-content-center mb-5 ${styles.pagination}`}
+          >
+            <ReactPaginate
+              previousLabel={""}
+              nextLabel={""}
+              breakLabel={"..."}
+              breakClassName={"break-me"}
+              pageCount={props.pagination.totalPage} // Total page
+              marginPagesDisplayed={2}
+              pageRangeDisplayed={2}
+              onPageChange={(e) => handlePageClick(e)}
+              containerClassName={styles.pagination}
+              subContainerClassName={`${styles.pages} ${styles.pagination}`}
+              activeClassName={styles.active}
+            />
+          </div>
         </section>
       </div>
       <Footer />
