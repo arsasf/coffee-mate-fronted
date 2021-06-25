e899a605bb96f4f1b790608f1fc8cbaf4cfd81d4
twicks95 <teguh.wck95@gmail.com>
2021-06-25 16:19:25 +0700

    Update product detail page

--- pages/customers/product-details/[id].js
+++ pages/customers/product-details/[id].js
@@ -13,7 +13,6 @@ export async function getServerSideProps(context) {
   const data = await authPage(context);
   await customerPage(context);
   const { id } = context.query;
-  console.log(data);
 
   const user = await axiosApiIntances
     .get(`user/by-id/${data.userId}`)
@@ -44,9 +43,7 @@ export async function getServerSideProps(context) {
 }
 
 export default function ProductDetails(props) {
-  console.log(props);
-
-  const [size, setSize] = useState("");
+  const [size, setSize] = useState("R");
   const [price, setPrice] = useState(props.product.product_base_price);
   const [count, setCount] = useState(1);
   const data = props.product.product_size.split(", ");
@@ -66,64 +63,67 @@ export default function ProductDetails(props) {
       productQty: count,
       productSize: size,
     };
-    console.log(data);
     axiosApiIntances.post("cart/", data).then((res) => {
       console.log(res);
     });
   };
-
+  const [initialPrice, setInitialPrice] = useState(
+    props.product.product_base_price
+  );
   const handleProductSize = (event) => {
     console.log(event);
     setCount(1);
     if (event === "R") {
       setPrice(props.product.product_base_price);
+      setInitialPrice(props.product.product_base_price);
       setSize("R");
     } else if (event === "L") {
       setPrice(props.product.product_base_price + 3000);
+      setInitialPrice(props.product.product_base_price + 3000);
       setSize("L");
     } else {
       setPrice(props.product.product_base_price + 5000);
+      setInitialPrice(props.product.product_base_price + 5000);
       setSize("XL");
     }
   };
 
   const handleQuantityIncrease = () => {
     setCount(count + 1);
-    setPrice(price * (count + 1));
+    setPrice(price + initialPrice);
   };
 
   const handleQuantityDecrease = () => {
     if (count > 1) {
       setCount(count - 1);
-      setPrice(price * (count - 1));
+      setPrice(price - initialPrice);
     }
   };
-
-  console.log(price);
 
   return (
     <div>
       <Layout title="Product Details">
         <Navbar product={true} login={true} />
-        <div className={`${styles.container} container-fluid`}>
-          <Row xs={1} lg={2} className="mb-3 mb-mb-0 gy-3">
+        <div className={`${styles.container}`}>
+          <Row xs={1} lg={2} className="mb-3 mb-mb-0 gy-3 py-5">
             <Col xs={12} md={4} lg={5} className={styles.left}>
               <p className={styles.textPromo}>
                 Favorite &amp; Promo
-                <span className={styles.left_column_text_1}>
-                  &gt; {props.product.product_name}
-                </span>
+                <span>&gt; {props.product.product_name}</span>
               </p>
-              <div className={`${styles.row} row`}>
-                <div className="row justify-content-center">
+              <div className={`${styles.row}`}>
+                <div className={styles.lgImageContainer}>
                   <img
-                    src="/default-cold-brew.png"
+                    src={
+                      props.product.product_image
+                        ? `http://localhost:3005/backend5/api/${props.product.product_image}`
+                        : "/default-img-placeholder.png"
+                    }
                     alt="coffee brew"
-                    className={`${styles.left_column_image} rounded-circle`}
                   />
                 </div>
               </div>
-              <div className={`${styles.row} row`}>
+              <div className={`${styles.row} row my-5`}>
                 <h1 className={styles.textDrink}>
                   {props.product.product_name}
                 </h1>
@@ -138,47 +138,46 @@ export default function ProductDetails(props) {
               </div>
             </Col>
             <Col xs={12} md={8} lg={7} className={styles.right}>
-              <div className={styles.boxRight1}>
-                <p className={styles.textRight1}>
-                  {props.product.product_desc}
-                </p>
-                <h5 className={styles.textRight2}>Choose a size</h5>
+              <div className={styles.chooseSizeSection}>
+                <p>{props.product.product_desc}</p>
+                <h5>Choose a size</h5>
                 <div className={styles.boxSize}>
-                  {data.map((item, index) => {
+                  {data.map((size, index) => {
                     return (
                       <button
                         key={index}
                         className={`${styles.right_circle_button} btn-primary rounded-circle`}
-                        onClick={() => handleProductSize(item)}
+                        onClick={() => handleProductSize(size)}
                       >
-                        {item}
+                        {size}
                       </button>
                     );
                   })}
                 </div>
               </div>
-              <div className={`${styles.boxRight2}`}>
-                <div className="col-3">
+              <div className={`${styles.checkoutSection}`}>
+                <div className={styles.smImageContainer}>
                   <img
-                    src="/default-cold-brew.png"
+                    src={
+                      props.product.product_image
+                        ? `http://localhost:3005/backend5/api/${props.product.product_image}`
+                        : "/default-img-placeholder.png"
+                    }
                     alt="coffee brew"
-                    className={`${styles.right_circle_image} rounded-circle`}
                   />
                 </div>
-                <div className={styles.boxCounter}>
-                  <h4 className={styles.textRight4}>
-                    {props.product.product_name}
-                  </h4>
-                  <div className={styles.buttonCounter}>
+                <div className={styles.countProduct}>
+                  <h4>{props.product.product_name}</h4>
+                  <div className={styles.btnCount}>
                     <button
-                      className={`${styles.buttonMinus} btn btn-primary`}
+                      className={`${styles.minus} btn btn-primary`}
                       onClick={handleQuantityDecrease}
                     >
                       -
                     </button>
                     <p className={styles.size}>{count}</p>
                     <button
-                      className={`${styles.buttonPlus} btn btn-primary`}
+                      className={`${styles.plus} btn btn-primary`}
                       onClick={handleQuantityIncrease}
                     >
                       +
@@ -186,9 +185,9 @@ export default function ProductDetails(props) {
                   </div>
                 </div>
                 <div className={styles.boxCheckout}>
-                  <h4 className={styles.textRight3}>Checkout</h4>
+                  <h4>Checkout</h4>
                   <button
-                    className={`${styles.buttonPlus} btn btn-primary`}
+                    className={`${styles.btnCheckout} btn btn-primary`}
                     onClick={handleCheckOut}
                   >
                     <AiOutlineArrowRight className={styles.arrow} />
