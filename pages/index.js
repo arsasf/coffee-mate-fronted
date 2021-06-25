c6bc455a2904ffcddb477fab8d2e635441679e0a
twicks95 <teguh.wck95@gmail.com>
2021-06-25 17:28:23 +0700

    Update home page

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,21 @@ import Layout from "components/Layout";
 import Navbar from "components/module/Navbar";
 import styles from "styles/Home.module.css";
 import { unauthPage } from "middleware/authPage";
+import Cookie from "js-cookie";
 
-export const getServerSideProps = async (context) => {
-  await unauthPage(context);
-  return {
-    props: {},
-  };
-};
+// export const getServerSideProps = async (context) => {
+//   await unauthPage(context);
+//   return {
+//     props: {},
+//   };
+// };
 
 export default function Home() {
+  const token = Cookie.get("token");
+
   return (
     <Layout title="Home">
-      <Navbar login={false} />
+      <Navbar login={token ? true : false} />
       <div className={styles.container}>
         <h1>Home</h1>
       </div>
